be43f7d2cc854d4b474394f923f3ce233e79058f
Sha1kh4 <adnan.shaikh66@mit.asia>
2025-06-17 17:44:54 +0530

    refactor: fixes to skills component

--- app/components/resume/Cloud.js
+++ app/components/resume/Cloud.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 const Cloud = () => {
   return (
@@ -10,31 +10,88 @@ const Cloud = () => {
         Cloud Computing
       </h3>
       <div className="resume-item">
-        <h4>Clouds(AWS-Amazon Web Services/Microsoft Azure/GCP-Google Cloud Platform)</h4>
+        <h4>
+          Clouds(AWS-Amazon Web Services/Microsoft Azure/GCP-Google Cloud
+          Platform)
+        </h4>
         <ul>
           <li>
-            <b>Design Architecture</b>: Develop and design scalable, resilient, and secure cloud architectures that meet business requirements.
+            <b>Design Architecture</b>: Develop and design scalable, resilient,
+            and secure cloud architectures that meet business requirements.
           </li>
           <li>
-            <b>Technical Guidance</b>: Provide technical leadership and guidance on AWS services and best practices.
+            <b>Technical Guidance</b>: Provide technical leadership and guidance
+            on AWS services and best practices.
           </li>
           <li>
-            <b>Cost Management</b>: Optimize solutions for cost-efficiency and provide cost estimates and budget management.
+            <b>Cost Management</b>: Optimize solutions for cost-efficiency and
+            provide cost estimates and budget management.
           </li>
           <li>
-            <b>Security</b>: Implement best practices for security and compliance in AWS environments.
+            <b>Security</b>: Implement best practices for security and
+            compliance in AWS environments.
           </li>
           <li>
-            <b>Integration</b>: Ensure seamless integration of AWS services with existing systems and applications.
+            <b>Integration</b>: Ensure seamless integration of AWS services with
+            existing systems and applications.
           </li>
           <li>
-            <b>Documentation</b>: Create and maintain detailed architecture diagrams, documentation, and white papers.
+            <b>Documentation</b>: Create and maintain detailed architecture
+            diagrams, documentation, and white papers.
           </li>
           <li>
-            <b>Stakeholder Communication</b>: Work with stakeholders to understand business requirements and translate them into technical solutions.
+            <b>Stakeholder Communication</b>: Work with stakeholders to
+            understand business requirements and translate them into technical
+            solutions.
           </li>
           <li>
-            <b>High-Availability Architectures</b>: Design solutions with high availability using AWS services like Elastic Load Balancing (ELB), Auto Scaling, and Amazon Route 53.
+            <b>High-Availability Architectures</b>: Design solutions with high
+            availability using AWS services like Elastic Load Balancing (ELB),
+            Auto Scaling, and Amazon Route 53.
+          </li>
+        </ul>
+      </div>{" "}
+      <div className="resume-item">
+        <h4>DevOps</h4>
+        <ul>
+          {/* <!-- <li>AI & Data Science(ML,DL, NN, NLU, NLP, TensorFlow, OpenCV)</li> -->
+                <!-- <li>Big Data & Data Lake(Hadoop, Spark & Databricks)</li> --> */}
+          <li>Clouds(AWS, Microsoft Azure & Google Cloud Platform).</li>
+          <li>
+            Operation (DevSysCloudOps, Baremetal, Virtualization, Cloud
+            Serverless).
+          </li>
+          <li>
+            Dev. (Full Stack Web using DJango Enterprise Level Application Dev.,
+            Design & Architecture)
+          </li>
+          <li>Version Control tools: Github, Bitbucket, GitLab </li>
+          <li>Container Management tools: Docker, Kubernets, Docker Swam </li>
+          <li>Application Monitoring Tools: Prometheus, Dynatrace, </li>
+          <li>Deployment & Server Monitoring tools: Splunk, Datadog </li>
+          <li>Configuration Management tools: Chef, Puppet, Ansible </li>
+          <li>CI / Deployment Automation tool: Jenkins, </li>
+          <li>Test Automation tools: Selenium, </li>
+          <li>Artifact Management tools: CloudRepo</li>
+          <li>Realse Management tools: Octopus Deploy, Plutora. Chef</li>
+          <li>Implement and manage CI/CD pipelines.</li>
+          <li>
+            {" "}
+            Automate deployment processes and infrastructure provisioning.
+          </li>
+          <li> Monitor and troubleshoot system performance and reliability.</li>
+          <li>
+            Collaborate with development and operations teams to ensure smooth
+            deployment and integration.
+          </li>
+          <li>
+            {" "}
+            Maintain and manage infrastructure using tools like Terraform,
+            Ansible, or Puppet.
+          </li>
+          <li>
+            {" "}
+            Ensure security practices are incorporated into the DevOps pipeline.
           </li>
         </ul>
       </div>
@@ -42,4 +99,4 @@ const Cloud = () => {
   );
 };
 
-export default Cloud; 
+export default Cloud;
--- app/components/skill/Programming.js
+++ app/components/skill/Programming.js
@@ -1,30 +1,58 @@
-'use client';
+"use client";
 
 const Programming = () => {
   return (
     <>
-      <h3 className="resume-title" style={{ paddingBottom: 10 }}>
+      <h3 className="resume-title">
         <div className="icon-size">
-          <img src="assets/img/icons/code-icon-vector.jpg" alt="Programming Languages" />
+          <img
+            src="assets/img/icons/code-icon-vector.jpg"
+            alt="Programming Languages"
+          />
         </div>
         Computer Programming Languages
       </h3>
       <div className="resume-item pb-0">
         <h4>
-          <img className="tool-icon" src="assets/img/icons/sub-list/programming.png" alt="" />
+          <img
+            className="tool-icon"
+            src="assets/img/icons/sub-list/programming.png"
+            alt=""
+          />
           Programming/Scripting
         </h4>
         <ul>
           <li>
             Python (From basics to Advance)
             <ul>
-              <li>Language Specification(Descriptors, Properties, Class Decorators, Metaclasses, Mixin Classes, Composition, Dynamic Class Creation)</li>
-              <li>OOP, Design Patterns, Memory Optimization, Unit Testing with Mockito</li>
-              <li>Frameworks(Flask, Fast.ai, Django, Pyramid), FastAPI (ASG), Gunicorn (WSGI)</li>
+              <li>
+                Language Specification(Descriptors, Properties, Class
+                Decorators, Metaclasses, Mixin Classes, Composition, Dynamic
+                Class Creation)
+              </li>
+              <li>
+                OOP, Design Patterns, Memory Optimization, Unit Testing with
+                Mockito
+              </li>
+              <li>
+                Frameworks(Flask, Fast.ai, Django, Pyramid), FastAPI (ASG),
+                Gunicorn (WSGI)
+              </li>
               <li>Boto3, AWS CDK, Anaconda, Jupyter</li>
-              <li><b>Numpy</b>(Array Creation, Manipulation, Universal Functions, Indexing, Slicing, Masking, Filtering, Broadcasting, Iteration, Mathematical Operations, Linear Algebra, Masked Arrays, Memory Management, Vectorization, Performance Optimization)</li>
-              <li><b>Pandas</b>(MultiIndex, Window functions, Method chaining, Custom accessor methods, Query optimization)</li>
-              <li><b>AI/ML/DL/Gen AI Libraries</b>(TensorFlow, PyTorch, JAX, Hugging Face Transformers, DeepSpeed)</li>
+              <li>
+                <b>Numpy</b>(Array Creation, Manipulation, Universal Functions,
+                Indexing, Slicing, Masking, Filtering, Broadcasting, Iteration,
+                Mathematical Operations, Linear Algebra, Masked Arrays, Memory
+                Management, Vectorization, Performance Optimization)
+              </li>
+              <li>
+                <b>Pandas</b>(MultiIndex, Window functions, Method chaining,
+                Custom accessor methods, Query optimization)
+              </li>
+              <li>
+                <b>AI/ML/DL/Gen AI Libraries</b>(TensorFlow, PyTorch, JAX,
+                Hugging Face Transformers, DeepSpeed)
+              </li>
             </ul>
           </li>
           <li>
@@ -50,15 +78,259 @@ const Programming = () => {
           <li>
             Big Data Scala &amp; R
             <ul>
-              <li>Classes &amp; Objects, Language Specifications, Closures, Collections, Traits</li>
-              <li>Patterns, RegEx, Extractors, File IO, Packages, OOPs, Params Types, Exceptions</li>
-              <li>Map, Annotations, Options, Evaluations, Streaming, Serializations, etc.</li>
+              <li>
+                Classes &amp; Objects, Language Specifications, Closures,
+                Collections, Traits
+              </li>
+              <li>
+                Patterns, RegEx, Extractors, File IO, Packages, OOPs, Params
+                Types, Exceptions
+              </li>
+              <li>
+                Map, Annotations, Options, Evaluations, Streaming,
+                Serializations, etc.
+              </li>
             </ul>
           </li>
+        </ul>
+      </div>
+      <h3 className="resume-title">
+        <div className="icon-size">
+          <img src="assets/img/icons/database.png" alt="Databases" />
+        </div>
+        Data Bases
+      </h3>
+      <div className="resume-item">
+        <h4>
+          <img
+            className="tool-icon"
+            src="assets/img/icons/sub-list/db-2.jpeg"
+            alt=""
+          />
+          SQL &amp; PL/SQL
+        </h4>
+        <ul>
+          <li>SQL (DML, DDL, SQL Operators, Joins, Sub Queries, Trigger)</li>
+          <li>
+            PL/SQL(Cursor, Exceptions, Functions, procedures, Indexes,
+            Sequences, Views &amp; Others Database Objects)
+          </li>
+        </ul>
+      </div>
+      <div className="resume-item">
+        <h4>
+          <img
+            className="tool-icon"
+            src="assets/img/icons/sub-list/db-2.jpeg"
+            alt=""
+          />
+          Key-Value Stores
+        </h4>
+        <ul>
+          <li>Redis</li>
+          <li>Riak</li>
+          <li>Amazon DynamoDB</li>
+        </ul>
+      </div>
+      <div className="resume-item">
+        <h4>
+          <img
+            className="tool-icon"
+            src="assets/img/icons/sub-list/db-2.jpeg"
+            alt=""
+          />
+          Time Series Databases
+        </h4>
+        <ul>
+          <li>InfluxDB</li>
+          <li>TimescaleDB</li>
+          <li>Prometheus</li>
+          <li>OpenTSDB</li>
+        </ul>
+      </div>
+      <div className="resume-item">
+        <h4>
+          <img
+            className="tool-icon"
+            src="assets/img/icons/sub-list/db-2.jpeg"
+            alt=""
+          />
+          Distributed File Systems / Big Data Storage
+        </h4>
+        <ul>
+          <li>Hadoop HDFS</li>
+          <li>Amazon S3</li>
+          <li>Google Cloud Storage</li>
+        </ul>
+      </div>
+      <div className="resume-item">
+        <h4>
+          <img
+            className="tool-icon"
+            src="assets/img/icons/sub-list/db-2.jpeg"
+            alt=""
+          />
+          Search Engine Databases
+        </h4>
+        <ul>
+          <li>Elasticsearch</li>
+          <li>Apache Solr</li>
+        </ul>
+      </div>
+      <div className="resume-item">
+        <h4>
+          <img
+            className="tool-icon"
+            src="assets/img/icons/sub-list/db-2.jpeg"
+            alt=""
+          />
+          Vector Databases
+        </h4>
+        <ul>
+          <li>Pinecone</li>
+          <li>Weaviate</li>
+          <li>FAISS (by Facebook)</li>
+          <li>Milvus</li>
+          <li>Qdrant</li>
+          <li>Vespa</li>
+        </ul>
+      </div>
+      <h3 className="resume-title">
+        <div className="icon-size">
+          <img
+            src="assets/img/icons/icons8-google-compute-engine-250.png"
+            alt="Mujahed Hussaini"
+          />
+        </div>
+        Operating System's
+      </h3>
+      <div className="resume-item">
+        <h4>
+          <img
+            className="tool-icon"
+            src="assets/img/icons/sub-list/os1.png"
+            alt=""
+          />
+          OS
+        </h4>
+        <ul>
+          <li>UNIX,Linux (Ubuntu, Alpine, CentOS, RedHat).</li>
+          <li>Mac.</li>
+          <li>Windows(3.0 - 11).</li>
+        </ul>
+      </div>
+      <h3 className="resume-title">
+        <div className="icon-size">
+          <img src="assets/img/icons/blockchain.png" alt="Mujahed Hussaini" />
+        </div>
+        Enterprise Data Ecosystems:
+      </h3>
+      <div className="resume-item">
+        <h4>
+          <img
+            className="tool-icon"
+            src="assets/img/icons/sub-list/rdbms.png"
+            alt=""
+          />
+          RDBMS
+        </h4>
+        <ul>
+          <li>MySQL (National level Project -NPR)</li>
+          <li>SQL Server</li>
+          <li>PostgreSQL</li>
+          {/* Add more list items as needed */}
+        </ul>
+      </div>
+      <div className="resume-item">
+        <h4>
+          <img
+            className="tool-icon"
+            src="assets/img/icons/sub-list/dw.png"
+            alt=""
+          />
+          Data warehouse
+        </h4>
+        <ul>
+          <li>Cores (OLAP Cubes, Table partitioning, etc)</li>
+          <li>SnowFlake</li>
+          <li>Azure Synapse Analytics</li>
+          <li>GCP Big Query</li>
+          <li>Azure OneLake Fabric</li>
+          <li>AWS Redshift</li>
+          {/* Add more list items as needed */}
+        </ul>
+      </div>
+      <h3 className="resume-title">
+        <div className="icon-size">
+          <img src="assets/img/icons/bigData.png" alt="Mujahed Hussaini" />
+        </div>
+        Big Data Technologies
+      </h3>
+      <div className="resume-item">
+        <h4>
+          <img
+            className="tool-icon"
+            src="assets/img/icons/sub-list/apache_hadoop_logo_icon_169586.png"
+            alt=""
+          />
+          Hadoop
+        </h4>
+        <ul>
+          <li>Apache Hadoop &amp; its Ecosystem</li>
+          <li>Architectures of MapReduce &amp; HDFS</li>
+          <li>Ambari Administration</li>
+          <li>Cloudera Administration</li>
+          {/* Add more list items as needed */}
+        </ul>
+      </div>
+      <div className="resume-item">
+        <h4>
+          <img
+            className="tool-icon"
+            src="assets/img/icons/sub-list/spark.png"
+            alt=""
+          />
+          Spark:
+        </h4>
+        <ul>
+          <li>Core, PySpark, SQL Spark, Scala Spark.</li>
+          <li>DAG Schedular, Block Manager</li>
+          <li>Broadcast and Accumulators</li>
+          <li>Catalyst &amp; Cost based Optimizer</li>
+          <li>Disk Spills, Dynamic Memory Allocation</li>
+          {/* Add more list items as needed */}
+        </ul>
+      </div>
+      <div className="resume-item">
+        <h4>
+          <img
+            className="tool-icon"
+            src="assets/img/icons/sub-list/Azure Databricks.png"
+            alt=""
+          />
+          Databricks (Basic to Advance):
+        </h4>
+        <ul>
+          <li>Architecture E2 Runtime</li>
+          <li>Unity Catalog</li>
+          <li>
+            Execution Plan (Physical, Unresolved, Resolved, Spark Execution
+            Plans){" "}
+          </li>
+          <li>Memory (Block, Heap, Off Heap, DMA, Disk Spills)</li>
+          <li>CI/CD on DataBricks</li>
+          <li>ML Deployments</li>
+          <li>Delta Lake house &amp; Engine</li>
+          <li>
+            MLFlow, Machine Learning Model drifting &amp; tracking- monitoring
+            &amp; worked on ML Model performance
+          </li>
+          <li>Implementations of Best Practices &amp; patterns.</li>
+          {/* Add more list items as needed */}
         </ul>
       </div>
     </>
   );
 };
 
-export default Programming; 
+export default Programming;
