29fa4b9dd44803a07565d4f3e76cc173c8c50f1c
Sha1kh4 <adnan.shaikh66@mit.asia>
2025-06-18 15:07:50 +0530

    refactor : import all required files instead of linking

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,23 @@
+// app/layout.js (or .tsx)
 import { Open_Sans, Raleway, Poppins } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import Preloader from "./components/Preloader";
-import "../public/assets/owlcarousel/assets/owl.carousel.min.css";
-import "../public/assets/owlcarousel/assets/owl.theme.default.min.css";
+
+// Vendor & Gallery Styles (move all CSS imports here)
+import "../public/assets/vendor/bootstrap/css/bootstrap.min.css";
+import "../public/assets/vendor/bootstrap-icons/bootstrap-icons.css";
+import "../public/assets/vendor/boxicons/css/boxicons.min.css";
+import "../public/assets/vendor/glightbox/css/glightbox.min.css";
+import "../public/assets/vendor/swiper/swiper-bundle.min.css";
+
+import "../public/assets/css/style.css";
+import "../public/assets/css/myAnimation.css";
+
+import "../public/assets/gallery-assets/css/bootstrap.min.css";
+import "../public/assets/gallery-assets/css/slicknav.min.css";
+import "../public/assets/gallery-assets/css/fresco.css";
+import "../public/assets/gallery-assets/css/slick.css";
 
 const openSans = Open_Sans({
   subsets: ["latin"],
@@ -30,9 +44,13 @@ export const metadata = {
   title: "Mujahed Hussaini - Home",
   description:
     "I possess a proven track record in analyzing intricate datasets.",
+  openGraph: {
+    title: "Mujahed Hussaini",
+    description: "Mujahed Hussaini Portfolio",
+  },
   keywords: ["IT Consultant"],
   icons: {
-    icon: "/assets/img/favicon.png",
+    icon: "/favicon.ico",
     apple: "/assets/img/apple-touch-icon.png",
   },
   openGraph: {
@@ -43,102 +61,25 @@ export const metadata = {
     charset: "utf-8",
   },
 };
+
 export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
       className={`${openSans.variable} ${raleway.variable} ${poppins.variable}`}
     >
-      <head>
-        {/* Vendor & Gallery CSS */}
-        {/* <link rel="stylesheet" href="/assets/vendor/aos/aos.css" /> */}
-        <link
-          rel="stylesheet"
-          href="/assets/vendor/bootstrap/css/bootstrap.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="/assets/vendor/bootstrap-icons/bootstrap-icons.css"
-        />
-        <link
-          rel="stylesheet"
-          href="/assets/vendor/boxicons/css/boxicons.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="/assets/vendor/glightbox/css/glightbox.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="/assets/vendor/swiper/swiper-bundle.min.css"
-        />
-
-        <link rel="stylesheet" href="/assets/css/style.css" />
-        <link rel="stylesheet" href="/assets/css/myAnimation.css" />
-
-        <link
-          rel="stylesheet"
-          href="/assets/gallery-assets/css/bootstrap.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="/assets/gallery-assets/css/font-awesome.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="/assets/gallery-assets/css/slicknav.min.css"
-        />
-        <link rel="stylesheet" href="/assets/gallery-assets/css/fresco.css" />
-        <link rel="stylesheet" href="/assets/gallery-assets/css/slick.css" />
+      <body>
         <link rel="stylesheet" href="/assets/gallery-assets/css/style.css" />
 
-        <link
-          rel="stylesheet"
-          href="/assets/owlcarousel/assets/owl.carousel.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="/assets/owlcarousel/assets/owl.theme.default.min.css"
-        />
-      </head>
-
-      <body>
         <Preloader />
         {children}
-        {/* Scripts placed with Next.js Script component */}
-        {/* <Script
-          src="/assets/gallery-assets/js/jquery.slicknav.min.js"
-          strategy="afterInteractive"
-        />
-        <Script src="/assets/js/main.js" strategy="afterInteractive" />
-        <Script src="/assets/js/music-contols.js" strategy="afterInteractive" />
-        <Script src="/assets/js/Contact.js" strategy="afterInteractive" /> */}
-        {/* Gallery Scripts */}
+        {/* Global Scripts */}
+
         <Script
+          defer
           src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
           strategy="beforeInteractive"
         />
-        {/* <Script
-          src="/assets/gallery-assets/js/jquery.slicknav.min.js"
-          strategy="afterInteractive"
-        />{" "} */}
-        {/* 
-        <Script
-          src="/assets/gallery-assets/js/slick.min.js"
-          strategy="afterInteractive"
-        /> */}
-        {/* <Script
-          src="/assets/gallery-assets/js/fresco.min.js"
-          strategy="afterInteractive"
-        />
-        <Script
-          src="/assets/gallery-assets/js/main.js"
-          strategy="afterInteractive"
-        />
-        <Script
-          src="/assets/gallery-assets/js/vendor/jquery-3.2.1.min.js"
-          strategy="beforeInteractive"
-        /> */}
       </body>
     </html>
   );
