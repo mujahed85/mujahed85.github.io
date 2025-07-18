4b267723057c15937f8182bc21197497527bbbd2
sha1kh4 <adnan.shaikh166@mit.asia>
2025-07-18 15:13:55 +0530

    feat: add Header component for navigation with links to various sections

---
+++ app/components/blog/header.js
@@ -0,0 +1,77 @@
+// Header.js
+import React from "react";
+
+export default function Header() {
+  return (
+    <header id="header" className="d-flex flex-column justify-content-center">
+      <nav id="navbar" className="navbar nav-menu">
+        <ul>
+          <li>
+            <a
+              href="/#hero"
+              className={"nav-link "} 
+            >
+              <i className="bx bx-home"></i> <span>Home</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="/#about"
+              className={"nav-link "}
+            >
+              <i className="bx bx-user"></i> <span>About</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="/#resume"
+              className={"nav-link "}
+            >
+              <i className="bx bx-book-content"></i> <span>Profile</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="/#Skill"
+              className={"nav-link "}
+            >
+              <i className="bx bx-shield-quarter"></i> <span>Skill's</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="/#gallery"
+              className={"nav-link "}
+            >
+              <i className="bx bx-image-add"></i> <span>Gallery</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="/#blog"
+              className={"nav-link "}
+            >
+              <i className="bx bxl-blogger"></i> <span>Blog's</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="/#services"
+              className={"nav-link "}
+            >
+              <i className="bx bx-server"></i> <span>Services</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="/#contact"
+              className={"nav-link "}
+            >
+              <i className="bx bx-envelope"></i> <span>Contact</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
